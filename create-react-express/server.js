ca970c175aeef4a7bf3843d1886fe63f0407afb7
Valentine <abraval@gmail.com>
2019-12-19 23:59:21 -0500

    updated server info

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,27 +1,21 @@
 const express = require("express");
-const path = require("path");
-
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
+const PORT = process.env.PORT || 3001;
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Use apiRoutes
-app.use("/api", apiRoutes);
+app.use(routes);
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlesearch", {useNewUrlParser: true});
 
+// Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
