ad6d727e9ffbe9d26cca87617b0096a1097a11af
Valentine <abraval@gmail.com>
2019-12-21 21:03:30 -0500

    minor styling fix

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron() {
     return (
             <div className="jumbotron text-center jumbotron-fluid">
                 <h1 className="jumbo display-4 py-auto">BOOK FINDER</h1>
-                <span className="py-2 px-3">powerd by <a className="google" href="https://developers.google.com/books" target="_blank">Google Books API</a></span>
+                <span className="googleName py-2 px-3">powerd by <a className="google" href="https://developers.google.com/books" target="_blank">Google Books API</a></span>
             </div>
     )
 }
